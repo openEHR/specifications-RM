9f181a093742eb2ee71235bc805101936b857914
wolandscat <thomas.beale@openehr.org>
2017-08-17 10:47:49 +0100

    SPECPUB-3 update - improvements to diagrams; more use of UML attribute qualifiers.

--- do_uml_generate-spec-RM.sh
+++ do_uml_generate-spec-RM.sh
@@ -1,3 +1,4 @@
 rm docs/UML/classes/*.*
 rm docs/UML/diagrams/*.*
-./uml_generate.sh -i {rm_release} -o docs/UML computable/UML/openEHR_UML-RM.mdzip
+./uml_generate.sh -i {rm_release} -r BASE -o docs/UML computable/UML/openEHR_UML-RM.mdzip
+./uml_generate.sh -i {rm_release} -r RM -o docs/UML computable/UML/openEHR_UML-RM.mdzip
