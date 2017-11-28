36d31d4001584cb1229cd7f675de3ba7e0d506b9
wolandscat <thomas.beale@openehr.org>
2017-11-28 15:21:50 +0000

    SPECPUB-6. Correct UML package nesting and paths in documents.
    Add global class index.

--- do_uml_generate-spec-RM.sh
+++ do_uml_generate-spec-RM.sh
@@ -1,4 +1,5 @@
 rm docs/UML/classes/*.*
 rm docs/UML/diagrams/*.*
+
 # ./uml_generate.sh -i {rm_release} -r BASE -o docs/UML computable/UML/openEHR_UML-RM.mdzip
 ./uml_generate.sh -i {rm_release} -r RM -o docs/UML computable/UML/openEHR_UML-RM.mdzip
