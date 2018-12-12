21d46914d834b46a345d0092f6209bea85f3c003
wolandscat <thomas.beale@openehr.org>
2018-12-12 21:34:35 +0000

    Republish 1.0.3 release with new link vars.

--- do_uml_generate-spec-RM.sh
+++ do_uml_generate-spec-RM.sh
@@ -1,1 +1,5 @@
-./uml_generate_win.sh -i {rm_release} -o docs/UML computable/UML/openEHR_UML-RM.mdzip
+rm docs/UML/classes/*.*
+rm docs/UML/diagrams/*.*
+
+# ./uml_generate.sh -i {rm_release} -r BASE -o docs/UML computable/UML/openEHR_UML-RM.mdzip
+./uml_generate.sh -i {rm_release} -r RM -o docs/UML computable/UML/openEHR_UML-RM.mdzip
