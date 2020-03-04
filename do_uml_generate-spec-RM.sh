19e5482ffcb8f2f0c31aa1d12316a41dc563025c
wolandscat <thomas.beale@openehr.org>
2020-03-04 21:36:48 +0000

    Publishing and minor formatting regeneration.

--- do_uml_generate-spec-RM.sh
+++ do_uml_generate-spec-RM.sh
@@ -1,5 +1,4 @@
 rm -f docs/UML/classes/*.*
 rm -f docs/UML/diagrams/*.*
 
-# ./uml_generate.sh -i {rm_release} -r BASE -o docs/UML computable/UML/openEHR_UML-RM.mdzip
-../specifications-AA_GLOBAL/bin/uml_generate.sh -i {rm_release} -r RM -o docs/UML computable/UML/openEHR_UML-RM.mdzip
+../specifications-AA_GLOBAL/bin/uml_generate.sh -d svg -i {rm_release} -r RM -o docs/UML computable/UML/openEHR_UML-RM.mdzip
